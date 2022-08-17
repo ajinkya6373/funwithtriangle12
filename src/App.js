67cbdf779dcae9552124e5e599ffd22e607a9fb4
ajinkya <ajinkyajadhav80586@gmail.com>
2022-08-17 19:29:40 +0530

    firstversion

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
 
+
+import { NavBar} from "./components"
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import {
+  Quiz,
+  IsTriangle,
+  Hypotenuse,
+  Area
+} from "./pages"
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+      <Router >
+        <NavBar />
+        <Routes>
+          <Route path="/" element={<IsTriangle />}  />
+          <Route path="/quiz" element={<Quiz />} />
+          <Route path="/hypotenuse" element={<Hypotenuse />} />
+          <Route path="/area" element={<Area />} />
+        </Routes>
+    
+      </Router>
+
     </div>
   );
 }
